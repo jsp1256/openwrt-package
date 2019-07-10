bb027c6600b3d255192b31daac4ee59912380005
jsp1256 <18711346878@163.com>
2019-07-10 21:40:02 +0800

    添加lib_ufsd.o_shipped函数屏蔽脚本，屏蔽ufsdvfs.o的函数

---
+++ ufsd/ufsd94/shell/progess_lib.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#负责解析函数列表并对.o文件的函数做替换
+
+a=`cat ufsdvfs.txt | awk '{print $6}'`
+
+if [ ! -f "libufsd_mipsel.o_shipped" ]; then
+	echo "libufsd_mipsel.o_shipped文件不存在，退出"
+	exit 1
+fi
+
+for i in $a
+do
+	mipsel-openwrt-linux-objcopy libufsd_mipsel.o_shipped --redefine-sym "$i"="$i""_hide"
+done
